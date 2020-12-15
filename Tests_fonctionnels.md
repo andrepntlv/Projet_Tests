26b7bc145c266d07b8941b4b3a3fd4e11d395adf
PINTO_ALVES_AN-ESIG <elv-andre.pntlv@eduge.ch>
2020-12-15 11:57:16 +0100

    1er test ajout d'un produit sans être connecté que client sur le site

---
+++ Tests_fonctionnels.md
@@ -0,0 +1,16 @@
+# Test 1
+Ajouter un article au panier sans être connecté
+Scénario
+"Aller sur la page produitEntier.php
+Séléctionner une quantité,
+Cliquer sur le bouton ajouter au panier"
+# Résultat attentu
+"Vous devez être conncté
+pour ajouter cet article
+à votre panier."
+# Test 2
+Sélectionner un produit, entrer une quatité supérieure à la quantité en stock
+# Test 3
+Sélectionner un produit, entrer la valeur 0 à la quantité en stock
+# Test 4
+Sélectionner un produit, entrer une valeur supérieure à 0 et inf| Cas de tests                                                                                                                    | Scénario                                                                                                                                                       | Données de tests | Résultat attendu                                                    |
--- test_fonctionnel_gererPanier.py
+++ test_fonctionnel_gererPanier.py
@@ -1,20 +1,35 @@
 from selenium import webdriver
 from time import sleep
+import unittest
 
-# Connexion en tant que client au site Yatara Massages
-def loginClient():
-    driver = webdriver.Chrome("drivers\chromedriver.exe")
-    driver.maximize_window()
-    driver.get("https://www.esig-sandbox.ch/team20_1_v2/connexion.php")
-    name = driver.find_element_by_id("fname")
-    name.send_keys("inlo")
-    password = driver.find_element_by_id("lname")
-    password.send_keys("inlo1234")
-    login = driver.find_element_by_name("formconnexion")
-    login.click()
-    sleep(100000)
+class testpanier(unittest.TestCase):
 
+    def setUp(self):
+        self.driver = webdriver.Chrome(r'C:\Users\PINTO_ALVES_AN-ESIG\PycharmProjects\inlo-projet-testing-andre-pntlv\drivers\chromedriver.exe')
 
+    # Connexion en tant que client au site Yatara Massages
+    def loginClient(self):
+        self.driver = webdriver.Chrome("drivers\chromedriver.exe")
+        self.driver.maximize_window()
+        self.driver.get("https://www.esig-sandbox.ch/team20_1_v2/connexion.php")
+        name = self.driver.find_element_by_id("fname")
+        name.send_keys("inlo")
+        password = self.driver.find_element_by_id("lname")
+        password.send_keys("inlo1234")
+        login = self.driver.find_element_by_name("formconnexion")
+        login.click()
+        sleep(10)
+
+    # Test sur l'ajout d'un produit sans être connecté
+    def test_notConnected(self):
+        self.driver.get("http://esig-sandbox.ch/team20_1_v2/produitEntier.php?id=3")
+        self.driver.find_element_by_name("quantite").send_keys(2)
+        self.driver.find_element_by_name("ajoutPanier").click()
+        self.driver.implicitly_wait(5)
+        error = self.driver.find_element_by_class_name(".card .card-cascade .wider .reverse")
+        #error = self.driver.find_element_by_class_name("card card-cascade wider reverse")
+        assert "Vous devez être connecté pour ajouter cet article à votre panier." in error
+        sleep(15000)
 
 if __name__ == '__main__':
-    loginClient()
+    unittest.main()
