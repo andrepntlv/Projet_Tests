70d1c87bc297bc9cbda723495b6ae4417669aef7
PINTO_ALVES_AN-ESIG <elv-andre.pntlv@eduge.ch>
2020-12-22 12:22:21 +0100

    2 premiers tests fonctionnels

--- Tests_fonctionnels.md
+++ Tests_fonctionnels.md
@@ -1,5 +1,13 @@
 # Test 1
-Ajouter un article au panier sans être connecté
+1. Ajouter un article au panier sans être connecté
+2. Sélectionner un produit, entrer une quatité supérieure à la quantité en stock
+3. Sélectionner un produit, sans entrer de valeur  de quantité et ajouter
+4. Sélectionner un produit, entrer une valeur en stock et commander
+
+
+
+
+
 Scénario
 "Aller sur la page produitEntier.php
 Séléctionner une quantité,
@@ -8,9 +16,3 @@ Cliquer sur le bouton ajouter au panier"
 "Vous devez être conncté
 pour ajouter cet article
 à votre panier."
-# Test 2
-Sélectionner un produit, entrer une quatité supérieure à la quantité en stock
-# Test 3
-Sélectionner un produit, entrer la valeur 0 à la quantité en stock
-# Test 4
-Sélectionner un produit, entrer une valeur supérieure à 0 et inf| Cas de tests                                                                                                                    | Scénario                                                                                                                                                       | Données de tests | Résultat attendu                                                    |
--- test_fonctionnel_gererPanier.py
+++ test_fonctionnel_gererPanier.py
@@ -20,6 +20,15 @@ class testpanier(unittest.TestCase):
         login.click()
         sleep(3)
 
+    # Def ajout d'un article dans le panier
+    def ajoutArticlePanier(self):
+        self.loginClient()
+        self.driver.find_element_by_xpath('//a[contains(@href,"produit.php")]').click()
+        self.driver.find_element_by_xpath('//a[contains(@href,"produitEntier.php?id=3")]').click()
+        self.driver.find_element_by_name("quantite").send_keys(15)
+        self.driver.find_element_by_name("ajoutPanier").click()
+
+
     # Test sur l'ajout d'un produit sans être connecté
     def test_notConnected(self):
         self.driver.get("http://esig-sandbox.ch/team20_1_v2/produitEntier.php?id=3")
@@ -29,6 +38,7 @@ class testpanier(unittest.TestCase):
         erreur = self.driver.find_element_by_xpath("//font").text
         self.assertEqual(erreur,"Vous devez être connecté pour ajouter cet article à votre panier.")
 
+    #Test sur l'ajout d'un produit sans choisir une valeur dans l'input
     def test_inputEmpty(self):
         self.loginClient()
         self.driver.find_element_by_xpath('//a[contains(@href,"produit.php")]').click()
@@ -36,6 +46,33 @@ class testpanier(unittest.TestCase):
         self.driver.find_element_by_name("ajoutPanier").click()
         erreur = self.driver.find_element_by_xpath("//font").text
         self.assertEqual(erreur, "Il faut rentrer une valeur pour ajouter au panier.")
+
+    #Test s'il n'y a pas assez de quantité en stock
+    def test_quantiteStock(self):
+        self.loginClient()
+        self.driver.find_element_by_xpath('//a[contains(@href,"produit.php")]').click()
+        self.driver.find_element_by_xpath('//a[contains(@href,"produitEntier.php?id=3")]').click()
+        self.driver.find_element_by_name("quantite").send_keys(50)
+        self.driver.find_element_by_name("ajoutPanier").click()
+        erreur = self.driver.find_element_by_xpath("//font").text
+        self.assertEqual(erreur, "Il n'y a pas assez de quantité en stock.")
+
+    #Test de l'ajout d'un produit fonctionnel
+    def test_ajoutProduit(self):
+        self.loginClient()
+        self.driver.find_element_by_xpath('//a[contains(@href,"produit.php")]').click()
+        self.driver.find_element_by_xpath('//a[contains(@href,"produitEntier.php?id=3")]').click()
+        self.driver.find_element_by_name("quantite").send_keys(15)
+        self.driver.find_element_by_name("ajoutPanier").click()
+        erreur = self.driver.find_element_by_xpath("//font").text
+        self.assertEqual(erreur, "Vous avez bien ajouté cet article à votre panier.")
+
+    def test_commander(self):
+        self.ajoutArticlePanier()
+        self.driver.find_element_by_xpath('//a[contains(@href,"panier.php")]').click()
+        prix = self.driver.find_element_by_xpath("//tr[4]/td[2]").text
+        self.assertEqual(prix, "315")
+
 
 if __name__ == '__main__':
     unittest.main()
