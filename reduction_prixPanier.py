79c7ca804c1f1f7d4970b31449ee207c3f484678
André <elv-andre.pntlv@eduge.ch>
2021-01-14 21:25:42 +0100

    10 tests unitaires + modification de la fonction à tester

--- reduction_prixPanier.py
+++ reduction_prixPanier.py
@@ -3,29 +3,29 @@ reduction = 0
 def reduction_prixPanier(panier):
     prixTotal = []
     global reduction
-    for prod in panier:
+    for prod in panier: # Kevin Bonga, aide pour la boucle en WEBA
         listQuantite = int(prod.get("quantity"))
         listPrix = prod.get("price")
 
-        totalLigne = float(listPrix) * listQuantite
-        prixTotal.append(totalLigne)
+        if type(listQuantite) == int and type(listPrix) == float:
+            totalLigne = float(listPrix) * listQuantite
+            prixTotal.append(totalLigne)
+        else:
+            raise TypeError('La quantité doit être en int, les prix en float.')
 
     prixTotal = sum(prixTotal)
-    if type(prixTotal) != float:
-        raise TypeError('Le montant doit être en type float.')
-    elif type(prixTotal) < 0:
+
+    if (prixTotal) and (listQuantite) and (listPrix) < 0:
         raise TypeError('Le montant doit être positif.')
-    elif type(listQuantite) < 0:
-        raise TypeError('Les quantités doivent être positifs.')
     else:
         if (prixTotal >= 1000):
-            prixTotal*80/100
+            prixTotal=prixTotal*80/100
             reduction = prixTotal*20/100
         elif (prixTotal >= 500 and prixTotal < 1000):
-            prixTotal * 90 / 100
+            prixTotal=prixTotal * 90 / 100
             reduction = prixTotal*10/100
         elif (prixTotal >= 250 and prixTotal < 500):
-            prixTotal * 95 / 100
+            prixTotal=prixTotal * 95 / 100
             reduction = prixTotal*5/100
         else:
             prixTotal
--- test_unitaire_reductionPanier.py
+++ test_unitaire_reductionPanier.py
@@ -1,20 +1,91 @@
 import unittest
-
 from reduction_prixPanier import reduction_prixPanier
-__import__(reduction_prixPanier())
-
-from reductio_prixPanier import reduction_prixPanier
 
 
 class MyTestCase(unittest.TestCase):
-    def test_montantSuperieurAMille(self):
+    # 4 tests "happy path"
+    def test_montantSuperieurA1000(self):
         panier = (
             [{'quantity': 4, 'price': 200.00},
              {'quantity': 3, 'price': 100.00}]
         )
         montant = reduction_prixPanier(panier)
-        self.assertEqual(1100, montant)
+        self.assertEqual(880, montant)
 
+    def test_montantSuperieurA500(self):
+        panier = (
+            [{'quantity': 2, 'price': 200.00},
+             {'quantity': 2, 'price': 100.00}]
+        )
+        montant = reduction_prixPanier(panier)
+        self.assertEqual(540, montant)
+
+    def test_montantSuperieurA250(self):
+        panier = (
+            [{'quantity': 1, 'price': 200.00},
+             {'quantity': 1, 'price': 60.00}]
+        )
+        montant = reduction_prixPanier(panier)
+        self.assertEqual(247, montant)
+
+    def test_montantSansReduction(self):
+        panier = (
+            [{'quantity': 4, 'price': 20.00},
+             {'quantity': 2, 'price': 10.00}]
+        )
+        montant = reduction_prixPanier(panier)
+        self.assertEqual(100, montant)
+
+    # 4 tests problématiques
+    def test_mauvaisTypeString(self):
+        panier = (
+            [{'quantity': 8, 'price': "test"},
+             {'quantity': 9, 'price': 89.00}]
+        )
+        with self.assertRaises(TypeError): # Dylan Monteiro aide pour la syntaxe
+            reduction_prixPanier(panier)
+
+    def test_mauvaisTypeBoolean(self):
+        panier = (
+            [{'quantity': 12, 'price': 50},
+             {'quantity': False, 'price': 50}]
+        )
+        with self.assertRaises(TypeError):
+            reduction_prixPanier(panier)
+
+    def test_prixNegatif(self):
+        panier = (
+            [{'quantity': 4, 'price': -50.00},
+             {'quantity': 1, 'price': -20.00}]
+        )
+        # montant = reduction_prixPanier(panier)
+        with self.assertRaises(TypeError):
+            reduction_prixPanier(panier)
+
+    def test_montantValeurCharniere(self):
+        panier = (
+            [{'quantity': 4, 'price': 50.00},
+             {'quantity': 1, 'price': 49.99}]
+        )
+        montant = reduction_prixPanier(panier)
+        self.assertEqual(249.99, montant)
+
+    # 2 tests cas extrêmes
+    def test_montantPanierEnorme(self):
+        panier = (
+            [{'quantity': 100, 'price': 5000.00},
+             {'quantity': 1000, 'price': 100.00}]
+        )
+        montant = reduction_prixPanier(panier)
+        self.assertEqual(480000, montant)
+
+    def test_montantPanierToutPetit(self):
+        panier = (
+            [{'quantity': 1, 'price': 0.05},
+             {'quantity': 5, 'price': 0.01}]
+        )
+        montant = reduction_prixPanier(panier)
+        self.assertEqual(0.10, montant)
 
 if __name__ == '__main__':
     unittest.main()
